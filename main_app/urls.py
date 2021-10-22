4a004bb8d2e9095fb8bc9f8e991ecdf6cb624bc8
dbhinton <dhinton.us@gmail.com>
2021-10-21 23:51:26 -0400

    added class based views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
-    path('assets/', views.crypto_index, name='index'),
-    path('assets/<int:crypto_id>/', views.crypto_detail, name = 'detail')
+    path('index/', views.crypto_index, name='index'),
+    path('assets/<int:crypto_id>/', views.crypto_detail, name = 'detail'),
+    path('assets/create/', views.CryptoCreate.as_view(), name = 'crypto_create'),
+    path('crypto/<int:pk>/update', views.CryptoUpdate.as_view(), name = 'crypto_update'),
+    path('crypto/<int:pk>/delete', views.CryptoDelete.as_view(), name = 'crypto_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from .models import Crypto
 
-# Add the following import
-from django.http import HttpResponse
+# # Add the following import
+# from django.http import HttpResponse
 
 # Define the home view
 def home(request):
@@ -18,4 +19,19 @@ def crypto_index(request):
 
 def crypto_detail(request, crypto_id):
   crypto = Crypto.objects.get(id = crypto_id)
-  return render(request, 'assets/details.html', { 'crypto': crypto })
+  return render(request, 'assets/detail.html', { 'crypto': crypto })
+
+class CryptoCreate(CreateView):
+  model = Crypto
+  fields = '__all__'
+  success_url = '/index/'
+
+
+class CryptoUpdate(UpdateView):
+  model = Crypto
+  fields = ['price']
+
+
+class CryptoDelete(DeleteView):
+  model = Crypto
+  success_url = '/index/'  
